a7f089ee11bbf82e958e74371bf72aee85b246d1
mre <m.reiprecht@conterra.de>
2020-10-09 08:57:38 +0200

    Prevent injection into non html pages Remove injected script to hide it from document

--- src/js/spy/inject.ts
+++ src/js/spy/inject.ts
@@ -3,9 +3,15 @@ if (window["$apprt"]) {
     console.warn("$apprt already defined, please reload the window.");
 } else {
     (function () {
+        if (!document.contentType || !document.contentType.startsWith("text/html")){
+            return;
+        }
+        if (document.documentElement && !/html/i.test(document.documentElement.tagName)){
+            return;
+        }
         var s = document.createElement("script");
         s.textContent = '(' + createSpy + ')();';
         (document.head || document.documentElement).appendChild(s);
-        //s.remove();
+        s.remove();
     })();
 }
